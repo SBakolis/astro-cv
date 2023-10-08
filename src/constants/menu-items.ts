d4b4f8f5f2cc66665fd1407f9ab062b921feea0d
SBakolis <s.bakolis@outlook.com>
2023-10-09 00:17:00 +0300

    header components

---
+++ src/constants/menu-items.ts
@@ -0,0 +1,19 @@
+import type MenuItem from "../types/menu-item";
+
+export const MenuItems: MenuItem[] = [
+  {
+    title: "Profile",
+    text: "Profile",
+    url: "#",
+  },
+  {
+    title: "Experience",
+    text: "Experience",
+    url: "#",
+  },
+  {
+    title: "Projects",
+    text: "Projects",
+    url: "#",
+  },
+];
